358c90aab5d850d448d737eee541828c19f2b546
MonPham <demondhunter93@gmail.com>
2023-02-20 23:15:27 +0700

    .

--- script.sh
+++ script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # mande-chaind keys add wallet
-
+mkdir ${HOME}/.mande-chaind && mkdir ${HOME}/.mande-chaind/config
 mande-chaind init $NODE_NAME --chain-id mande-testnet-1
+
 wget https://raw.githubusercontent.com/mande-labs/testnet-1/main/genesis.json -O ${HOME}/.mande-chaind/config/genesis.json
 SEEDS="cd3e4f5b7f5680bbd86a96b38bc122aa46668399@34.171.132.212:26656"
 PEERS="ee8a1b98e931e81d32c52f0b489fa22b52778d7c@34.171.132.212:26656,6780b2648bd2eb6adca2ca92a03a25b216d4f36b@34.170.16.69:26656"
